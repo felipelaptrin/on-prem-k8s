aee9d74fa06fea645dd9879b2efc55d6f1dbe9f2
felipelaptrin <paotrindadepao@gmail.com>
2024-06-06 00:10:14 -0300

    remove comment

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -14,7 +14,6 @@ provider "rancher2" {
   alias = "bootstrap"
 
   api_url = "https://${var.rancher_endpoint}"
-  # token_key = data.external.fetch_rancher_token.result["output"]
   insecure  = true
   bootstrap = true
 }
